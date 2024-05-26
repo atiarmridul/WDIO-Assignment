3ad68ebad7ae19613e0d57cc85336ac7bcb2694b
atiarmridul <atiarmridul@gmail.com>
2024-05-26 16:01:13 +0600

    scrolling is done

--- test/specs/test.assignment 3.B.js
+++ test/specs/test.assignment 3.B.js
@@ -1,5 +1,5 @@
 
-describe ("This is 3rd Classes Assignemnt number 2", async()=>{
+describe ("This is 3rd Classes Assignemnt number 2 contains button click, scrolling, form fillup", async()=>{
 
 it ("Should get the add button, click it, then get the delete button and click it", async()=>{
 
@@ -12,11 +12,24 @@ it ("Should get the add button, click it, then get the delete button and click i
 
     const dltButton = await $("button[onclick='deleteElement()']")
     await dltButton.click();
-    await browser.pause(3000)
+    await browser.pause(10000)
 })
 
 it ("Should go to the specific website, scroll down once and scroll up again", async()=>{
 
+    await browser.url("https://demoqa.com/elements")
+    await browser.pause(3000)
+    const pageEndButtons = await $$('//span[@class="pr-1"]')[5]
+    await pageEndButtons.scrollIntoView()
+    await browser.pause(3000)
+
+    const pageStartButtons = await $$('//span[@class="pr-1"]')[0]
+    await pageStartButtons.scrollIntoView()
+    await browser.pause(3000)
+
+
 })
+
+
 
 })
