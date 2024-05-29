196c4f7ee6db516ae8fbc535532c258b831cdc1e
atiarmridul <atiarmridul@gmail.com>
2024-05-29 16:50:39 +0600

    window change

--- test/specs/test.assignment 4.js
+++ test/specs/test.assignment 4.js
@@ -5,12 +5,22 @@ describe ("This is 4th Classes Assignemnt which contains Window chnage and Alert
     
                 await browser.url("https://demoqa.com/links")
                 await browser.pause(3000);
-                const linkForNewTab = await $('#dynamicLink');
-                await linkForNewTab.click()
+                
+                // await browser.refresh
+                const linkForNewTab = await $('#simpleLink');
+                await linkForNewTab.click();
                 await browser.pause(3000);
-                const totalTabs = browser.getWindowHandles()
-                console.log("Total Tabs>>>>>",(await totalTabs).length)
+                const totalTabs = await browser.getWindowHandles()
+                console.log("Total Tabs>>>>>", totalTabs.length)
+                console.log("Tabs>>>>>",totalTabs)
                 await browser.pause(3000);
+                // await browser.closeWindow()
+                await browser.switchToWindow(totalTabs[1])
+                await browser.pause(5000);
+                const currentTabURL = await browser.getUrl()
+                console.log("Current Url ==",currentTabURL)
+   
+
 
 
                 
