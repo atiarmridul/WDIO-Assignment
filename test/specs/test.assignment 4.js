9c5afbc76a64ed3951702a9fcd6b26c7e3bff75b
atiarmridul <atiarmridul@gmail.com>
2024-05-29 17:34:55 +0600

    window implementation done

--- test/specs/test.assignment 4.js
+++ test/specs/test.assignment 4.js
@@ -5,6 +5,7 @@ describe ("This is 4th Classes Assignemnt which contains Window chnage and Alert
     
                 await browser.url("https://demoqa.com/links")
                 await browser.pause(3000);
+                await browser.refresh 
                 
                 // await browser.refresh
                 const linkForNewTab = await $('#simpleLink');
@@ -14,14 +15,18 @@ describe ("This is 4th Classes Assignemnt which contains Window chnage and Alert
                 console.log("Total Tabs>>>>>", totalTabs.length)
                 console.log("Tabs>>>>>",totalTabs)
                 await browser.pause(3000);
-                // await browser.closeWindow()
                 await browser.switchToWindow(totalTabs[1])
                 await browser.pause(5000);
-                const currentTabURL = await browser.getUrl()
-                console.log("Current Url ==",currentTabURL)
-   
+                const currentTabUrlChild = await browser.getUrl()
+                console.log("Current Child Url ==",currentTabUrlChild)
+                await browser.closeWindow()
+                await browser.pause(3000);
+                await browser.switchToWindow(totalTabs[0])
+                const currentTabUrlMother = await browser.getUrl()
+                console.log("Current Mother Url ==",currentTabUrlMother)
+                await browser.pause(3000);
 
-
+                
 
                 
 
