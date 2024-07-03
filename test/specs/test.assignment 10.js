056f56b4072cbaa89cf599a8639365f576dcb405
atiarmridul <atiarmridul@gmail.com>
2024-07-03 15:51:42 +0600

    shadow element assignment done

--- test/specs/test.assignment 10.js
+++ test/specs/test.assignment 10.js
@@ -6,13 +6,13 @@ describe ("This is 10th Classes Assignemnt", async()=>{
 
 
 
-        // it('Shadow Elements', async () => {
-        //     await browser.url('https://letcode.in/shadow');
-        //     await browser.pause(3000)
-        //     const firstName = await $('#open-shadow').shadow$('#fname');
-        //     await firstName.setValue("Atiar")
-        //     await browser.pause(3000)
-        // })
+        it('Shadow Elements', async () => {
+            await browser.url('https://letcode.in/shadow');
+            await browser.pause(3000)
+            const firstName = await $('#open-shadow').shadow$('#fname');
+            await firstName.setValue("Atiar")
+            await browser.pause(3000)
+        })
 
 // other 2 fields is under the #shadow-root (closed), so the elements are not accesable. 
 
@@ -25,22 +25,6 @@ describe ("This is 10th Classes Assignemnt", async()=>{
 
 
 
-
-        // Browser Events =====================================
-        it('Browser Events', async () => {
-            await browser.navigateTo('https://play1.automationcamp.ir/')
-            await browser.pause(3000)
-            await browser.url('https://letcode.in/test');
-            await browser.pause(3000)
-            await browser.back()
-            await browser.pause(3000)
-            await browser.forward()
-            await browser.pause(3000)
-            await browser.refresh()
-            await browser.pause(3000)
-    
-        })
-    
 
 })
 
